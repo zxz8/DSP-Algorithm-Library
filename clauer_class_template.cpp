67a00079f9969f4c928f77bb4ca3b64e6e1ffe77
Christoph Lauer <christophlauer@me.com>
2022-08-30 20:39:14 +0200

    add the algorithm class templates

---
+++ clauer_class_template.cpp
@@ -0,0 +1,47 @@
+/**
+ * (c)      Christoph Lauer Engineering
+ *
+ * @file    clauer_class_template.h
+ * @class   ClauerClassTemplate
+ * @version 1.0
+ * @date    2022
+ * @author  Christoph Lauer
+ * @brief   Class template file
+ * @see     http://en.wikipedia.org/...
+ * @todo    finished so far
+ */
+
+//------------------------------------------------------------------------------------------------
+ 
+// Local headers
+#include "clauer_class_template.h"
+
+// C Langunage Library headers
+#include <cmath>
+#include <cstdio>
+#include <cassert>
+
+// C++ Language Library headers
+#include <iostream>
+
+// Stl headers
+#include <string>
+ 
+//------------------------------------------------------------------------------------------------
+ 
+namespace clauer
+{
+namespace template
+{
+
+//------------------------------------------------------------------------------------------------
+ 
+void ClauerClassTemplate::templateFunction()
+{
+}
+ 
+//------------------------------------------------------------------------------------------------
+ 
+} // namespace template
+
+} // namepsace clauer
